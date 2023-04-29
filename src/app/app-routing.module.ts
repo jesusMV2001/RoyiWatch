641cd768ed5d32f7df142a46dde93e19383d1d1d
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2023-04-29 11:18:31 +0200

    First routing configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { DetailsComponent } from './details/details.component';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"index",component:IndexComponent},
+  {path:"details/:id",component:DetailsComponent},
+  {path:"form/:id",component:FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
