ef9762d177577bbadb4bea9eaf7c96e90fa5ce8c
jesusMV2001 <jesusmoralesvillegas@gmail.com>
2023-04-29 12:43:09 +0200

    Cabecera y footer html terminado

---
+++ src/app/components/cabecera/cabecera.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.css']
+})
+export class CabeceraComponent {
+  public titulo: String;
+
+  constructor(){
+    this.titulo = "DAW Angular";
+  }
+}
